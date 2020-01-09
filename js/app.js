9ca7e1247e104cc191c82bcf90c4b01a0477b114
Jen Merritt <jenjomez@gmail.com>
2020-01-09 15:09:35 +0000

    logged form submit and prevent default behaviour

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
 })
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+
+
+
+  this.reset();
+  console.log("it works");
+};
